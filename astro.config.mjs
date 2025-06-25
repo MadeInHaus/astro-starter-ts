e4b7c9aac582ae432b34ccb6040c824e2644ce70
Claus Wahlers <claus@codeazur.com.br>
2025-06-25 16:32:47 -0300

    Remove node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import * as dotenv from 'dotenv';
 import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node';
 
 dotenv.config();
 
@@ -9,16 +8,6 @@ dotenv.config();
 export default defineConfig({
     site: `https://${process.env.SITE_DOMAIN}`,
     build: { assets: '_static' },
-    output: process.env.PREVIEW === 'true' ? 'server' : 'static',
-    adapter: node({ mode: 'standalone' }),
     integrations: [sitemap()],
     devToolbar: { enabled: false },
-    // image: {
-    //     domains: ['example.com'],
-    //     experimentalLayout: 'constrained',
-    //     service: { entrypoint: 'astro/assets/services/sharp' },
-    // },
-    // experimental: {
-    //     responsiveImages: true,
-    // },
 });
